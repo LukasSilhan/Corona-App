611740086de4a336d68a24995e0134b9e5cd5881
Lukas Silhan <silhan.luk@gmail.com>
2020-04-23 00:28:40 +0200

    Updates

---
+++ precache-manifest.74498c58bde9c3e859b9b719f33c6892.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ebdbde090bfa546cff05296f61b97c4",
+    "url": "/Corona-App/index.html"
+  },
+  {
+    "revision": "be5e7077782369d0ba86",
+    "url": "/Corona-App/static/css/main.902472c7.chunk.css"
+  },
+  {
+    "revision": "dea9481c59f12665622a",
+    "url": "/Corona-App/static/js/2.4a33c24a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-App/static/js/2.4a33c24a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be5e7077782369d0ba86",
+    "url": "/Corona-App/static/js/main.c732a378.chunk.js"
+  },
+  {
+    "revision": "eb75ad2bc3d5aa602e2f",
+    "url": "/Corona-App/static/js/runtime-main.e283c665.js"
+  }
+]);
